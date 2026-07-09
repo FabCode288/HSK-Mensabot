77420de89788d2c238015be369b67390956695e3
FabCode288 <fab.konto@gmail.com>
2026-07-09 16:04:32 +0200

    Fine workung Navigation in Simulation

--- src/mensabot_bringup/launch/Simulation/sim_complete.launch.py
+++ src/mensabot_bringup/launch/Simulation/sim_complete.launch.py
@@ -1,14 +1,8 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    IncludeLaunchDescription,
-    TimerAction,
-)
-from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 
 
@@ -18,18 +12,11 @@ def generate_launch_description():
         'mensabot_bringup'
     )
 
-    mapping_arg = DeclareLaunchArgument(
-        'mapping',
-        default_value='False',
-        description='Start SLAM mapping instead of localization/navigation'
-    )
-
     sim_bringup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
                 pkg_mensabot_bringup,
-                'launch',
-                'Simulation',
+                'launch/Simulation',
                 'sim_bringup.launch.py'
             )
         )
@@ -39,13 +26,9 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(
                 pkg_mensabot_bringup,
-                'launch',
-                'Simulation',
+                'launch/Simulation',
                 'localization_sim_amcl.launch.py'
             )
-        ),
-        condition=UnlessCondition(
-            LaunchConfiguration('mapping')
         )
     )
 
@@ -53,27 +36,9 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(
                 pkg_mensabot_bringup,
-                'launch',
-                'Simulation',
+                'launch/Simulation',
                 'navigation_sim.launch.py'
             )
-        ),
-        condition=UnlessCondition(
-            LaunchConfiguration('mapping')
-        )
-    )
-
-    mapping_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                pkg_mensabot_bringup,
-                'launch',
-                'Simulation',
-                'mapping_sim.launch.py'
-            )
-        ),
-        condition=IfCondition(
-            LaunchConfiguration('mapping')
         )
     )
 
@@ -81,36 +46,18 @@ def generate_launch_description():
         period=3.0,
         actions=[
             localization_launch
-        ],
-        condition=UnlessCondition(
-            LaunchConfiguration('mapping')
-        )
+        ]
     )
 
     delayed_navigation = TimerAction(
         period=6.0,
         actions=[
             navigation_launch
-        ],
-        condition=UnlessCondition(
-            LaunchConfiguration('mapping')
-        )
-    )
-
-    delayed_mapping = TimerAction(
-        period=6.0,
-        actions=[
-            mapping_launch
-        ],
-        condition=IfCondition(
-            LaunchConfiguration('mapping')
-        )
+        ]
     )
 
     return LaunchDescription([
-        mapping_arg,
         sim_bringup_launch,
         delayed_localization,
         delayed_navigation,
-        delayed_mapping,
     ])
