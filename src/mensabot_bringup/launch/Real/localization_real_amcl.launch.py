b0bbce5435b1e2becfed07d9e298926011b21f05
FabCode288 <fab.konto@gmail.com>
2026-07-16 10:53:12 +0200

    New Map 16_07

--- src/mensabot_bringup/launch/Real/localization_real_amcl.launch.py
+++ src/mensabot_bringup/launch/Real/localization_real_amcl.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     map_file_path = os.path.join(
         pkg_mensabot_navigation,
         'maps',
-        'labor1307.yaml'
+        'Labor16_07.yaml'
     )
 
     amcl_localization_launch = IncludeLaunchDescription(
