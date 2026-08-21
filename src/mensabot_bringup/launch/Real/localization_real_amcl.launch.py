deb17b74eafc484c16c26c0993f86e4baaf23f56
FabCode288 <fab.konto@gmail.com>
2026-08-21 08:25:09 +0200

    New Map for PPT

--- src/mensabot_bringup/launch/Real/localization_real_amcl.launch.py
+++ src/mensabot_bringup/launch/Real/localization_real_amcl.launch.py
@@ -57,7 +57,7 @@ def generate_launch_description():
     map_file_path = os.path.join(
         pkg_mensabot_navigation,
         'maps',
-        'Labor_18_08.yaml'
+        'Labor2108.yaml'
     )
 
     amcl_localization_launch = IncludeLaunchDescription(
